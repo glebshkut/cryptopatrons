b678914e1930d42796aaedc557c86d27a8773005
Gleb Shkut <gleb.shkut@gmail.com>
2024-04-18 07:58:26 -0700

    remove unused react import

--- packages/nextjs/components/widgets/RecentDonations/index.tsx
+++ packages/nextjs/components/widgets/RecentDonations/index.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import React from "react";
 import { formatEther } from "viem";
 import { useScaffoldContractRead } from "~~/hooks/scaffold-eth";
 import { mainContractName } from "~~/lib/contract";
